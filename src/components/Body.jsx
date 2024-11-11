022756a1bbe8c604cd3871f04aa877a7fd4d00bb
Bharath <104580107+BharathkumarRangaraj@users.noreply.github.com>
2024-11-11 15:42:33 +0530

    reactrouterdom,componets and it's children

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./navBar";
+import Footer from "./Footer";
+
 const Body = () => {
-    return <div>Body</div>;
+    return <div>
+      <NavBar/>
+      <Outlet/>
+      <Footer/>
+    </div>;
   };
   export default Body;
   
