db2defb0916c02bc67f4bbff3a60a1a99c96b8cf
Bharath <104580107+BharathkumarRangaraj@users.noreply.github.com>
2024-11-15 09:57:05 +0530

    redirect to login if not authunticated with token

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,41 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./navBar";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/const";
+import { useDispatch, useSelector } from "react-redux";
+import { adduser } from "../utils/slices/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
-    return <div>
-      <NavBar/>
-      <Outlet/>
-      <Footer/>
-    </div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const userdata = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if (userdata) return;
+    try {
+      const res = await axios.get("http://localhost:7777/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(adduser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.log(err.message);
+    }
   };
-  export default Body;
-  
+  useEffect(() => {
+    fetchUser();
+  }, []);
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+export default Body;
